e091225e692267ca10da388f4a2f46cf57a28459
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-05-06 10:33:47 +0000

    Deploy

--- controllers/postController.js
+++ controllers/postController.js
@@ -46,6 +46,7 @@ exports.getPost = async (req, res) => {
 
 exports.createPost = async (req, res) => {
   try {
+    req.body.creator = req.user._id
     const newPost = await Post.create(req.body);
     res.status(201).json({
       status: "Post was successfully created",
