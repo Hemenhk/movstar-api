9fe1d4e7fc9c811bae8d38f75a439f9440e263ad
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-05-06 08:47:38 +0000

    Send token via cookie

--- models/postModel.js
+++ models/postModel.js
@@ -6,7 +6,7 @@ const postSchema = new mongoose.Schema(
     creator: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      // required: true,
     },
     title: {
       type: String,
