d3a5891df97113f6edcf343b089858b0292a2d1e
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-05-11 07:40:00 +0000

    Add more cors settings

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept-Encoding"],
+    exposedHeaders: ["Content-Encoding"]
   })
 );
 
