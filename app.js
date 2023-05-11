c13a2888aa6f5f862d6626e544408ce107d173fe
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-05-11 07:53:58 +0000

    Add xss

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const xss = require("express-xss-sanitizer")
+const xss = require("express-xss-sanitizer");
 const cookieParser = require("cookie-parser");
 const app = express();
 const movieRoutes = require("./routes/movieRoutes");
 const userRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(xss())
 
 app.use(
   cors({
@@ -25,6 +20,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "content-type");
   next();
 });
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(xss());
 
 // Routes
 
